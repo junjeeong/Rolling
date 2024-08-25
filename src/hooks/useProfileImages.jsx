a41f70ed4a76803bbbceda62889ab969deca6569
cccwon2 <cccwon2@naver.com>
2024-08-25 20:36:59 +0900

    Feat: useBackgroundImages, useProfileImages 동일한 데이터를 가져오는 커스텀 훅에 sessionStorage 적용

--- src/hooks/useProfileImages.jsx
+++ src/hooks/useProfileImages.jsx
@@ -7,8 +7,27 @@ const useProfileImages = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const responseData = await getProfileImages();
-      setProfileImages(responseData.imageUrls);
+      // sessionStorage에서 기존 데이터 가져오기
+      const cachedData = sessionStorage.getItem("profileImages");
+
+      if (cachedData) {
+        // 캐시된 데이터가 있으면 파싱하여 상태에 저장
+        setProfileImages(JSON.parse(cachedData));
+      } else {
+        // 캐시된 데이터가 없으면 API 호출하여 데이터 가져오기
+        try {
+          const responseData = await getProfileImages();
+          setProfileImages(responseData.imageUrls);
+
+          // sessionStorage에 데이터 저장
+          sessionStorage.setItem(
+            "profileImages",
+            JSON.stringify(responseData.imageUrls)
+          );
+        } catch (error) {
+          console.error("Failed to fetch profile images:", error);
+        }
+      }
     };
 
     fetchData();
