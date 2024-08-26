d0b8461e14185f4f887605527f1a9ea64dfc55b4
kim-miso <smilekim94@gmail.com>
2024-08-26 19:40:58 +0900

    Feat: Layout변수 수정

--- src/styles/DefaultLayout.jsx
+++ src/styles/DefaultLayout.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header.jsx';
 
-const Layout = styled.div`
+export const Layout = styled.div`
   max-width: 1200px;
   margin: 0 auto;
   padding: 20px;
