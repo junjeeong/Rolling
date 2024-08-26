dce674994757bafe71b4f971690e3fc0c09181db
junjeeong <wn8624@naver.com>
2024-08-27 02:33:33 +0900

    Feat, Chore : HeaderService.jsx에서 몇명이 작성했는지 보여주는 기능 추가, HeaderService에 message props 추가로 전달, 상태변수명 message에서 messages 복수형으로 변경

--- src/components/Header/HeaderService.jsx
+++ src/components/Header/HeaderService.jsx
@@ -1,12 +1,60 @@
-import styled from 'styled-components';
-import { EmojiSelector } from '../Emoji/EmojiSelector';
-import { EmojiTopBadge } from '../Emoji/EmojiTopBadge';
+import styled from "styled-components";
+import { EmojiSelector } from "../Emoji/EmojiSelector";
+import { EmojiTopBadge } from "../Emoji/EmojiTopBadge";
 const Container = styled.div`
   background-color: white;
   display: flex;
   justify-content: center;
   height: 68px;
   padding: 13px 0;
+`;
+
+const AuthorCountNoticeWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 228px;
+  height: 28px;
+  font-size: 18px;
+  font-weight: normal;
+`;
+
+const AuthorProfile = styled.img`
+  position: absolute;
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  border: 2px solid var(--white);
+
+  &:nth-child(1) {
+    z-index: 1;
+    margin-left: 16px;
+  }
+
+  &:nth-child(2) {
+    z-index: 2;
+    margin-left: 32px;
+  }
+
+  &:nth-child(3) {
+    z-index: 3;
+    margin-left: 48px;
+  }
+`;
+
+const OtherAuthors = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  border: 2px solid var(--gray-100);
+  background-color: var(--white);
+  font-size: 12px;
+  z-index: 4;
+  margin-left: 64px;
 `;
 
 const ServiceWrap = styled.div`
@@ -28,18 +76,39 @@ const RecipientInfo = styled.div`
 `;
 //getReactionsByRecipientId 함수를 사용하여 수신자의 이모지 정보를 가져올 예정
 
-export const HeaderService = ({ recipient }) => {
+export const HeaderService = ({ recipient, messages }) => {
   return (
     <Container>
       {recipient && (
-        <ServiceWrap>
-          <p>To: {recipient.name}</p>
-          <RecipientInfo>
-            <p>이모지 총 개수: {recipient.reactionCount}</p>
-            <EmojiTopBadge />
-            <EmojiSelector />
-          </RecipientInfo>
-        </ServiceWrap>
+        <>
+          <ServiceWrap>
+            <p>To: {recipient.name}</p>
+            <AuthorCountNoticeWrap>
+              <div style={{ width: "76px", position: "relative" }}>
+                {messages.map((message, index) =>
+                  index < 3 ? (
+                    <AuthorProfile key={index} src={message.profileImageURL} />
+                  ) : index === 3 ? (
+                    <OtherAuthors key={index}>
+                      +{messages.length - 3}
+                    </OtherAuthors>
+                  ) : null
+                )}
+              </div>
+              <span>
+                <span style={{ fontWeight: "bold" }}>
+                  {recipient.messageCount}
+                </span>
+                명이 작성했어요!
+              </span>
+            </AuthorCountNoticeWrap>
+            <RecipientInfo>
+              <p>이모지 총 개수: {recipient.reactionCount}</p>
+              <EmojiTopBadge />
+              <EmojiSelector />
+            </RecipientInfo>
+          </ServiceWrap>
+        </>
       )}
     </Container>
   );
