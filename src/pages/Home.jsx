d3c5e9a657e8e3a3324520212367d3e34e9acaad
cccwon2 <cccwon2@naver.com>
2024-08-27 19:52:37 +0900

    Chore: Home 카카오톡 및 URL 공유하기 테스트 제외

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,3 @@
-import ShareDropdown from "../components/Share/ShareDropdown";
-import useKakaoShare from "../hooks/useShareKakao";
-
 export const Home = () => {
-  // 카카오 SDK 로드용 커스텀 훅
-  const isKakaoReady = useKakaoShare();
-
-  return (
-    <div id="homeContainer" style={{ padding: "20px" }}>
-      <h1>Share Example</h1>
-      {isKakaoReady && <ShareDropdown />}
-    </div>
-  );
+  return <div>HomePage</div>;
 };
