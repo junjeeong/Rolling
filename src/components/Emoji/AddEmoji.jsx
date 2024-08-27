a4219ed4c7777777643850bff164d1ff72c48018
yyezzzy <t47047877@gmail.com>
2024-08-28 01:25:51 +0900

    feat: 이모티콘 선택 및 표시를 위한 AddEmoji 컴포넌트 추가

---
+++ src/components/Emoji/AddEmoji.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import OutlineButton from '../common/Button/OutlineButton';
+import plus from '../../assets/images/icons/emoji_plus.png';
+import { EmojiSelector } from './EmojiSelector';
+import { useEmojiPicker } from '../../hooks/useEmojiPicker';
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const EmojiBtnWrap = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  padding: 6px 16px;
+  color: var(--gray-900);
+  font-size: 16px;
+  font-weight: var(--font-medium);
+`;
+
+const EmojiWrap = styled.div`
+  position: absolute;
+  z-index: 2;
+  top: 100%;
+  right: 0;
+  height: 100px;
+`;
+
+export const AddEmoji = () => {
+  const { selectedEmoji, showPicker, setShowPicker, handleEmojiSelect, pickerRef } = useEmojiPicker(); // 커스텀 훅 사용
+
+  return (
+    <Container>
+      <OutlineButton onClick={() => setShowPicker(!showPicker)}>
+        <EmojiBtnWrap>
+          <img src={plus} alt="emoji_plus" width="24px" />
+          <p>{selectedEmoji || '추가'}</p>
+        </EmojiBtnWrap>
+      </OutlineButton>
+      <EmojiWrap>
+        <EmojiSelector showPicker={showPicker} onSelectEmoji={handleEmojiSelect} pickerRef={pickerRef} />
+      </EmojiWrap>
+    </Container>
+  );
+};
