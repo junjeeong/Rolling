084e56387dba73cfd2c3613f7d8e0e3937fe0ee3
cccwon2 <cccwon2@naver.com>
2024-08-27 11:11:37 +0900

    Test: Home 에 카카오톡 / URL 공유 테스트용 버튼 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,14 @@
+import ShareDropdown from "../components/Share/ShareDropdown";
+import useKakaoShare from "../hooks/useShareKakao";
+
 export const Home = () => {
-  return <div>HomePage</div>;
+  // 카카오 SDK 로드용 커스텀 훅
+  const isKakaoReady = useKakaoShare();
+
+  return (
+    <div id="homeContainer" style={{ padding: "20px" }}>
+      <h1>Share Example</h1>
+      {isKakaoReady && <ShareDropdown />}
+    </div>
+  );
 };
